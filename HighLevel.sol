5610b4821544ca3d51ce639a2fa5e8876f8a98a5
BlockHero <lomsadze.lazare2003@gmail.com>
2023-12-06 00:32:08 +0400

    High-Low Level

--- HighLevel.sol
+++ HighLevel.sol
@@ -1,1 +1,12 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract A {
+    uint sum;
+    function storeSum(B b) external {
+        sum = b.add(5, 10);
+    }
+}
+interface B {
+    function add(uint, uint) external returns(uint);
+}
